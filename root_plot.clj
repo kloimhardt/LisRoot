8712a019c97c8216873d6f16ffaf3c31edf13e17
kloimhardt <markus.agwin@gmx.net>
2023-07-03 14:50:19 +0200

    change plot function name

--- root_plot.clj
+++ root_plot.clj
@@ -5,10 +5,10 @@
 
 (c/load-types "root_types.edn")
 
-(defn plot-function [i]
-  (fn [[x]] (* i x)))
+(defn n-times-x [n]
+  (fn [[x]] (* n x)))
 
-(def pf1 ((c/new TF1) "f1" (plot-function 2) -5.001 5.0 2))
+(def pf1 ((c/new TF1) "f1" (n-times-x 2) -5.001 5.0 2))
 (def pc1 ((c/new TCanvas) "c1" "Something" 0 0 800 600))
 
 ((c/call TF1 Draw) pf1)
