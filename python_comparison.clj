20ed423c0eb8abe0b3a8e69728eb17596219910b
kloimhardt <markus.agwin@gmx.net>
2023-07-05 16:06:51 +0200

    higher order

--- python_comparison.clj
+++ python_comparison.clj
@@ -1,17 +1,30 @@
 (native-header "TCanvas.h")
 (native-header "TF1.h")
-(require '[c_interop :as c])
 
+(require '[c_interop :as c])
 (c/load-types "root_types.edn")
 
-(def f ((c/new TF1) "pyf2"
-        (fn [[x] [par0 par1]]
-          (+ par0 (* x par1)))
-        -1. 1. 2))
+(defn l []
+  (fn [[x] [par0 par1]]
+    (+ par0 (* x par1))))
+
+(def f ((c/new TF1) "pyf2" (l) -1. 1. 2))
 
 ((c/call TF1 SetParameters) f 5. 2.)
 
 (def c ((c/new TCanvas)))
 
 ((c/call TF1 Draw) f)
-((c/call TCanvas Print) c "python_comparison.pdf")
+((c/call TCanvas Print) c "pyf2.pdf")
+
+
+;; Example 2
+
+(def linear [par0 par1]
+  (fn [[x]]
+    (+ par0 (* x par1))))
+
+((c/call TF1 Draw)
+ ((c/new TF1) "pyf3" (linear 5. 2.) -1. 1. 2))
+
+((c/call TCanvas Print) c "pyf3.pdf")
--- root_plot.clj
+++ root_plot.clj
@@ -9,7 +9,7 @@
   (fn [[x]] (* n x)))
 
 (def pf1 ((c/new TF1) "f1" (n-times-x 2) -5.001 5.0 2))
-(def pc1 ((c/new TCanvas) "c1" "Something" 0 0 800 600))
+(def pc1 ((c/new TCanvas :B) "c1" "Something" 0 0 800 600))
 
 ((c/call TF1 Draw) pf1)
 ((c/call TCanvas Print) pc1 "c_interop_1.pdf")
@@ -17,7 +17,7 @@
 (c/add-type [:Classes TF1] [:B string string int int])
 
 (def pf2 ((c/new TF1 :B) "f2" "sin(x)" -5 5))
-(def pc2 ((c/new TCanvas) "c2" "Something" 0 0 800 600))
+(def pc2 ((c/new TCanvas :B) "c2" "Something" 0 0 800 600))
 
 ((c/call TF1 Draw) pf2)
 ((c/call TCanvas Print) pc2 "c_interop_2.pdf")
