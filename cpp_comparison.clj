15fae39b35f6e594618b0855ddafa90fde0ee108
kloimhardt <markus.agwin@gmx.net>
2023-07-15 22:40:04 +0200

    clean cpp_comparison

--- cpp_comparison.clj
+++ cpp_comparison.clj
@@ -41,25 +41,25 @@
     (* (apply f params)
        (apply g params))))
 
-(def Fnslits ((c/new TF1) "Fnslits" (fmul single nslit0) -5.001 5. 2))
+(def Fnslits ((TF1) "Fnslits" (fmul single nslit0) -5.001 5. 2))
 
-((c/call TF1 SetNpx) Fnslits 500)
-((c/call TF1 SetParameters) Fnslits 0.2 2)
+((TF1 SetNpx) Fnslits 500)
+((TF1 SetParameters) Fnslits 0.2 2)
 
-((c/call TF1 Draw) Fnslits)
-((c/call TCanvas Print) c "nslits.pdf")
+((TF1 Draw) Fnslits)
+((TCanvas Print) c "nslits.pdf")
 
 (c/add-signature [TF1 Eval] [:A double double])
 
 (def now1 (micros))
-(def erg ((c/call TF1 Eval) Fnslits 0.4))
+(def erg ((TF1 Eval) Fnslits 0.4))
 (println "Basetime: " erg (- (micros) now1))
 
 (c/add-signature [TF1 GetX]
                  [:A double double double double double int])
 
 (def now (micros))
-(def erg ((c/call TF1 GetX) Fnslits 3.6 -5.0 0.3 1.E-14 1000000000))
+(def erg ((TF1 GetX) Fnslits 3.6 -5.0 0.3 1.E-14 1000000000))
 (println "Calctime: " erg (- (micros) now))
 
 ;; Basetime:  40 +-10
