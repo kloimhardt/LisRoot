5f7d7762b050d6885dd6c54ac7c1c723806ee1c8
kloimhardt <markus.agwin@gmx.net>
2023-07-17 15:55:55 +0200

    add mallifns to interop

--- c_interop.clj
+++ c_interop.clj
@@ -45,6 +45,34 @@
   (add-type-raw (concat (list :Types) path) t)
   nil)
 
+(defmacro malli-fns []
+  (def remove-kw-ns
+    (fn [v]
+      (mapv (fn [x] (cond
+                      (vector? x) (remove-kw-ns x)
+                      (keyword? x) (keyword (name x))
+                      :else x))
+            v)))
+
+  (def malli-to-map
+    (fn [v]
+      (reduce (fn [acc pair]
+                (if (vector? (second pair))
+                  (if (= :multi (first (second pair)))
+                    (if (get-in acc (vector :Types :Classes (get-in (second pair) (vector 2 1 1 1 1))))
+                      (reduce (fn [cacc v]
+                                (assoc-in cacc (vector :Types :Classes (get-in v (vector 1 1 1 1)) (first pair) (first v)) (second v)))
+                              acc (nnext (second pair)))
+                      (assoc-in acc (vector :Types :Classes (first pair)) (into (hash-map) (nnext (second pair)))))
+                    (assoc-in acc (vector :Types :Functions (first pair)) 1))
+                  (assoc-in acc (vector :Types :Functions (first pair)) 1))
+                )
+              (hash-map) (partition 2 (remove-kw-ns v)))))
+
+  nil)
+
+(malli-fns)
+
 (defmacro class-fns []
   (def make-syms
     (fn [s n]
