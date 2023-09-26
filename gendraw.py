b0cdde483c6ca4bd1a373dbe97e463aa6f212089
kloimhardt <markus.agwin@gmx.net>
2023-09-26 13:02:32 +0200

    clean

--- gendraw.py
+++ gendraw.py
@@ -1,6 +1,8 @@
 import re
 
-csrc = """ void TF1::Draw	(	Option_t * 	option = ""	) """
+def mutcons(e, l):
+    l.insert(0, e)
+    return l
 
 def kw(s):
     return ":" + s
@@ -21,25 +23,24 @@ def cvtyp(s):
 def arg0(a20):
     optional_argument = True if "=" in a20 else False
     nameidx = -3 if optional_argument else -1
-    b0= [kwmap,
-         [kw("A"), [kwmap, [kwrtm, [kwmap]], [kwcxx, [kwcat]]]] if optional_argument else null,
-         [kw("B"), [kwmap,
-                    [kwrtm, [kwmap, [kw(a20[nameidx]), cvtyp(a20[0])]]],
-                    [kwcxx, [kwcat]]]]]
-    return b0
+    erg = [[kw("A"), [kwmap, [kwrtm, [kwmap]], [kwcxx, [kwcat]]]] if optional_argument else false,
+           [kw("B"), [kwmap,
+                      [kwrtm, [kwmap, [kw(a20[nameidx]), cvtyp(a20[0])]]],
+                      [kwcxx, [kwcat, cvtyp(a20[0])]]]]]
+    return erg
 
 def head(a01, a20):
-    return [kw(a01[0]), [kwmap, [kw(a01[1]), arg0(a20)]]]
+    return [kw(a01[0]), [kwmap, [kw(a01[1]), mutcons(kwmap, arg0(a20))]]]
 
 def malli(s):
     rp = re.compile("([()])")
     a = rp.split(s)
-    a2 = a[2].split(",")
-    a20 = a2[0].split()
     a0 = a[0].split()
     a01 = a0[1].split("::")
-    return head(a01, a20)
-
+    a2 = a[2].split(",")
+    a20 = a2[0].split()
+    erg = list(filter(lambda x: x, head(a01, a20)))
+    return erg
 
 def listostring(l):
     if type(l) is list:
@@ -47,4 +48,5 @@ def listostring(l):
     else:
         return l
 
+csrc = """ void TF1::Draw	(	Option_t * 	option = ""	) """
 print(listostring(malli(csrc)))
