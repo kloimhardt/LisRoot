4743a084817212d4bbc627d059b96bc45d69ed9b
kloimhardt <markus.agwin@gmx.net>
2024-06-16 14:32:10 +0200

    add comments to ptutorial.py

--- ptutorial.py
+++ ptutorial.py
@@ -20,10 +20,10 @@ def to_json(t):
 ys_header = "!yamlscript/v0\n"
 
 def to_clojure(code):
-    return yamlscript.YAMLScript().load(ys_header + "ys/compile(\"" + code + "\")").replace("+++", "identity")
+    return yamlscript.YAMLScript().load(ys_header + "ys/compile(\"" + code + "\")")
 
 # the following YAMLScript code is correctly translated to Clojure
-# but it has lots of backslashes in it
+# but it has lots of necessary backslashes in it
 
 not_minglable ="""
 doto:\\n
@@ -34,8 +34,8 @@ doto:\\n
 
 print(to_clojure(ys_header+not_minglable))
 
-# mingle_text() inserts the backspaces for the "yaml_code" below
-# but is not sophisticated enough for the above "not_minglable" case
+# mingle_text() inserts the necessary backslashes for the "yaml_code" below
+# but it is not sophisticated enough for the above "not_minglable" case
 
 def mingle_text(t):
     return t.replace(": ", ":\\ ").replace(":\n", ":\\n").replace("\n", "\n\n")
@@ -44,9 +44,6 @@ yaml_code = """
 native-header: 'ROOT.h'
 
 require cxx: => ROO
-
-ROO/def-ys-plus:
-ROO/def-ys-star:
 
 defn Linear():
   fn([x] [d k]): d + (k * x)
@@ -63,11 +60,27 @@ ROO/T(Draw TF1) f:
 ROO/T(Print TCanvas) c: 'ptutorial_2.pdf'
 """
 
+# YAML-code is data and surely can be shown as JSON structure
 print(to_json(yaml_code))
 
-clj_code = to_clojure(ys_header+mingle_text(yaml_code))
+# the mingled YAML-code needs a header to become YAMLScript-code
+ys_code = ys_header+mingle_text(yaml_code)
 
+# Clojure code is generated out of YAMLScript-code
+clojure_code = to_clojure(ys_code)
+
+# YAMLScript compiles to Clojure code that uses "+_" for arithmetic addition
+# Ferret, our Clojure-to-C++ compiler does not like that
+# to make amends we add a Clojure definition to the code
+ferret_code = "(def +_ +)" + clojure_code
+
+# same for "*_", but in a dirty way
+ferret_code = ferret_code.replace("*_", "*")
+
+# we again prefer it clean for the YAMLScript speciality "+++"
+ferret_code = "(def +++ identity)" + ferret_code
+
+# write Clojure code to file and shell out to Ferret and Clang compiler
 with open("temp.clj", "w") as text_file:
-    print(clj_code, file=text_file)
-
+    print(ferret_code, file=text_file)
 os.system("java -jar ferret.jar -i temp.clj && clang++ temp.cpp $(root-config --glibs --cflags --libs) -o temp && ./temp")
