6d18134d097a2945df81a30a37399e38ddadd4a1
kloimhardt <markus.agwin@gmx.net>
2024-06-17 12:41:23 +0200

    simplify ptutorial

--- ptutorial.py
+++ ptutorial.py
@@ -1,5 +1,7 @@
 import os, ROOT, yamlscript
 
+# example taken from https://root.cern/manual/python
+
 class Linear:
     def __call__(self, arr, par):
         return par[0] + arr[0]*par[1]
@@ -14,33 +16,10 @@ c = ROOT.TCanvas()
 f.Draw()
 c.Print("ptutorial_1.pdf");
 
-def to_json(t):
-    return yamlscript.YAMLScript().load(t)
+# YAMLScript code for the same example
 
-ys_header = "!yamlscript/v0\n"
-
-def to_clojure(code):
-    return yamlscript.YAMLScript().load(ys_header + "ys/compile(\"" + code + "\")")
-
-# the following YAMLScript code is correctly translated to Clojure
-# but it has lots of necessary backslashes in it
-
-not_minglable ="""
-doto:\\n
-  newTF1 'pyf2':\ l -1. 1. 2\n
-\ SetParameters:\ 5. 2.\n
-\ Draw:\
-"""
-
-print(to_clojure(ys_header+not_minglable))
-
-# mingle_text() inserts the necessary backslashes for the "yaml_code" below
-# but it is not sophisticated enough for the above "not_minglable" case
-
-def mingle_text(t):
-    return t.replace(": ", ":\\ ").replace(":\n", ":\\n").replace("\n", "\n\n")
-
-yaml_code = """
+ys_code = """
+!yamlscript/v0
 native-header: 'ROOT.h'
 
 require cxx: => ROO
@@ -52,33 +31,47 @@ l =: Linear()
 
 f =:
   ROO/T(new TF1) 'pyf2': l -1. 1. 2
+
 ROO/T(SetParameters TF1) f: 5. 2.
 
 c =:
   ROO/T(new TCanvas :empty):
+
 ROO/T(Draw TF1) f:
 ROO/T(Print TCanvas) c: 'ptutorial_2.pdf'
+
+newTF1 =: ROO/T(new TF1)
+SetParameters =: ROO/T(SetParameters TF1)
+Draw =: ROO/T(Draw TF1)
+
+doto:
+  newTF1 'pyf2': l -1. 1. 2
+  SetParameters: 5. 2.
+  Draw:
+
+ROO/T(Print TCanvas) c: 'ptutorial_3.pdf'
+
+=>: 'end'
 """
 
-# YAML-code is data and surely can be shown as JSON structure
-print(to_json(yaml_code))
-
-# the mingled YAML-code needs a header to become YAMLScript-code
-ys_code = ys_header+mingle_text(yaml_code)
+def to_clojure(code):
+    code = code.replace(": ", ":\\ ").replace("\n", "\\n")
+    return yamlscript.YAMLScript().load("!yamlscript/v0\nys/compile(\"" + code + "\")")
 
 # Clojure code is generated out of YAMLScript-code
 clojure_code = to_clojure(ys_code)
 
 # YAMLScript compiles to Clojure code that uses "+_" for arithmetic addition
-# Ferret, our Clojure-to-C++ compiler does not like that
-# to make amends we add a Clojure definition to the code
+# Ferret, our Clojure-to-C++ compiler does not like that, it wants simple "+"
+# to make amends we add the following specific Clojure definition to the code
 ferret_code = "(def +_ +)" + clojure_code
 
-# same for "*_", but in a dirty way
+# to make amends for the "*_", we use a more dirty way, simply because we can
 ferret_code = ferret_code.replace("*_", "*")
 
-# we again prefer it clean for the YAMLScript speciality "+++"
-ferret_code = "(def +++ identity)" + ferret_code
+# The YAMLScript speciality "+++" is also unknown to Ferret
+# and we remove it via the golden path of Lisp: a Macro
+ferret_code = "(defmacro +++ [e] e)" + ferret_code
 
 # write Clojure code to file and shell out to Ferret and Clang compiler
 with open("temp.clj", "w") as text_file:
