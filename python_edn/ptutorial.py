449766a95067fab54ebb9d77194d998c0e491f74
kloimhardt <markus.agwin@gmx.net>
2024-06-20 18:19:40 +0200

    add a Python macro expander

--- python_edn/ptutorial.py
+++ python_edn/ptutorial.py
@@ -1,38 +1,10 @@
-import os, ROOT
-
-# The following example is taken from https://root.cern/manual/python
-# it is translated to YAMLScript and executed using the ys command line tool
-# PRs are very welcome, also for the short text to be found in
-# https://github.com/kloimhardt/LisRoot/blob/main/paper/access_root_with_ys.md
-
-class Linear:
-    def __call__(self, arr, par):
-        return par[0] + arr[0]*par[1]
-
-# create a linear function with offset 5, and pitch 2
-l = Linear()
-f = ROOT.TF1('pyf2', l, -1., 1., 2)
-f.SetParameters(5., 2.)
-
-# plot the function
-c = ROOT.TCanvas()
-f.Draw()
-c.Print("ptutorial_1.pdf");
-
-g = ROOT.TF1('pyg2', 'sin(x * 10)')
-g.Draw()
-c.Print("ptutorial_2.pdf");
-
-# YAMLScript code for the same example
+import os, yamlscript, edn_format
 
 ys_code = """
 !yamlscript/v0
 native-header: 'ROOT.h'
 
 require cxx: => ROO
-
-ROO/def-ys-plus:
-ROO/def-ys-star:
 
 defn Linear():
   fn([x] [d k]): d + (k * x)
@@ -45,7 +17,7 @@ f =:
 ROO/T(SetParameters TF1) f: 5. 2.
 
 c =:
-  ROO/T(new TCanvas :empty):
+  ROO/T(make me a Canvas!):
 
 ROO/T(Draw TF1) f:
 ROO/T(Print TCanvas) c: 'ptutorial_3.pdf'
@@ -61,9 +33,41 @@ ROO/T(Print TCanvas) c: 'ptutorial_4.pdf'
 =>: 'end'
 """
 
-# the YAMLScript code is written to file and executed using ys, ferret and clang
+def to_clojure(code):
+    code = code.replace(": ", ":\\ ").replace("\n", "\\n")
+    return yamlscript.YAMLScript().load("!yamlscript/v0\nys/compile(\"" + code + "\")")
 
-with open("temp.ys", "w") as text_file:
-    print(ys_code, file=text_file)
+clojure_code = to_clojure(ys_code)
 
-os.system("ys -c temp.ys >temp.clj && java -jar ../ferret.jar -i temp.clj && clang++ temp.cpp $(root-config --glibs --cflags --libs) -o temp && ./temp")
+edn_code = edn_format.loads("(" + clojure_code + ")")
+
+def sym(s):
+    return edn_format.edn_lex.Symbol(s)
+
+def kw(s):
+    return edn_format.edn_lex.Keyword(s)
+
+def replaceCode (x):
+    if isinstance(x, tuple):
+        if x == (sym("ROO/T"), sym("make"), sym("me"), sym("a"), sym("Canvas!")):
+            return (sym("ROO/T"), sym("new"), sym("TCanvas"), kw("empty"))
+        if x[0] == sym("+++"):
+            return replaceCode(x[1])
+        if x[0] == sym("+_"):
+            return replaceCode((sym("+"), ) + x[1:])
+        if x[0] == sym("*_"):
+            return replaceCode((sym("*"), ) + x[1:])
+        else:
+            return tuple(map(replaceCode,x))
+    else:
+        return x
+
+# in the generated Clojure code, the functions +_, *_ and +++ need replacement
+modified_edn_code = replaceCode(edn_code)
+
+ferret_code = edn_format.dumps(modified_edn_code)[1:-1]
+
+with open("temp.clj", "w") as text_file:
+    print(ferret_code, file=text_file)
+
+os.system("java -jar ../ferret.jar -i temp.clj && clang++ temp.cpp $(root-config --glibs --cflags --libs) -o temp && ./temp")
