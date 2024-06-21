ad2ca730189e3fa08aaf3d5cd0e8e55708f8590a
kloimhardt <markus.agwin@gmx.net>
2024-06-21 14:33:46 +0200

    change ROOT/T to ROOT

--- python_edn/ptutorial.py
+++ python_edn/ptutorial.py
@@ -9,23 +9,23 @@ defn Linear():
 l =: Linear()
 
 f =:
-  ROO/T(new TF1) 'pyf2': l -1. 1. 2
+  ROOT(new TF1) 'pyf2': l -1. 1. 2
 
-ROO/T(SetParameters TF1) f: 5. 2.
+ROOT(SetParameters TF1) f: 5. 2.
 
 c =:
-  ROO/T(make me a Canvas!):
+  ROOT(make me a Canvas!):
 
-ROO/T(Draw TF1) f:
-ROO/T(Print TCanvas) c: 'ptutorial_3.pdf'
+ROOT(Draw TF1) f:
+ROOT(Print TCanvas) c: 'ptutorial_3.pdf'
 
-newTF1 =: ROO/T(new TF1 [string string])
-Draw =: ROO/T(Draw TF1)
+newTF1 =: ROOT(new TF1 [string string])
+Draw =: ROOT(Draw TF1)
 
 Draw:
   newTF1 'pyg2':: sin(x * 10)
 
-ROO/T(Print TCanvas) c: 'ptutorial_4.pdf'
+ROOT(Print TCanvas) c: 'ptutorial_4.pdf'
 
 =>: 'end'
 """
@@ -34,30 +34,29 @@ def to_clojure(code):
     code = code.replace(": ", ":\\ ").replace("\n", "\\n")
     return yamlscript.YAMLScript().load("!yamlscript/v0\nys/compile(\"" + code + "\")")
 
-def wrap_clojure(code):
-    return "=>: !clj |\n " + code
+def wrap_clojure_exp(exp):
+    return "=>: !clj |\n " + exp
 
-def compose2(f, g):
-    return lambda *a, **kw: f(g(*a, **kw))
+def macroexpand_clojure_exp(macro_code, code):
+    a = wrap_clojure_exp("(pr-str (macroexpand-1 '" + code + "))")
+    return yamlscript.YAMLScript().load("!yamlscript/v0\n" + macro_code + "\n" + a)
+
+def macroexpand_edn(macro_code_str, code_edn):
+    return edn_format.loads(macroexpand_clojure_exp(macro_code_str, edn_format.dumps(code_edn)))
 
 def readfile(filename):
     with open(filename, 'r') as file:
         str_code = file.read()
     return str_code
 
-def wrap_clojure_file(filename):
+def compose2(f, g):
+    return lambda *a, **kw: f(g(*a, **kw))
+
+def yaml_clojure_file(filename):
     str_code = readfile(filename)
     edn_code = edn_format.loads("(" + str_code + ")")
-    a = map(compose2(wrap_clojure, edn_format.dumps), edn_code)
+    a = map(compose2(wrap_clojure_exp, edn_format.dumps), edn_code)
     return '\n'.join(a)
-
-def macroexpand_clojure(macro_code, code):
-    a = wrap_clojure("(pr-str (macroexpand-1 '" + code + "))")
-    return yamlscript.YAMLScript().load("!yamlscript/v0\n" + macro_code + "\n" + a)
-
-def macroexpand_edn(macro_code_str, code_edn):
-    return edn_format.loads(macroexpand_clojure(macro_code_str, edn_format.dumps(code_edn)))
-
 
 clojure_code = to_clojure(ys_code)
 
@@ -69,13 +68,13 @@ def sym(s):
 def kw(s):
     return edn_format.edn_lex.Keyword(s)
 
-clojure_macros = wrap_clojure_file('lisroot_clojure_functions.clj') +"\n" + wrap_clojure_file('lisroot_clojure_macros.clj')
+clojure_macros = yaml_clojure_file('lisroot_clojure_functions.clj') +"\n" + yaml_clojure_file('lisroot_clojure_macros.clj')
 
 def replaceCode (x):
     if isinstance(x, tuple):
-        if x == (sym("ROO/T"), sym("make"), sym("me"), sym("a"), sym("Canvas!")):
-            return replaceCode((sym("ROO/T"), sym("new"), sym("TCanvas"), kw("empty")))
-        if x[0] == sym("ROO/T"):
+        if x == (sym("ROOT"), sym("make"), sym("me"), sym("a"), sym("Canvas!")):
+            return replaceCode((sym("ROOT"), sym("new"), sym("TCanvas"), kw("empty")))
+        if x[0] == sym("ROOT"):
             return macroexpand_edn(clojure_macros, (sym("T"), ) + x[1:])
         if x[0] == sym("+++"):
             return replaceCode(x[1])
